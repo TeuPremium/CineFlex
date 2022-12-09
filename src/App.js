3295ede095cf220cde6df05ff31ec6b46341cefe
Vinicius Brandao <vinicius.brandaot@gmail.com>
2022-12-09 19:39:50 -0300

    tela selecao filmes e loading

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Section from './components/Section';
 import { useState } from 'react';
 import axios from 'axios';
 import Loading from './components/Loading';
+import styled from 'styled-components';
 
 function App() {
   const [section, setSection]=useState('Selecione o filme')
   
-  const changeSection = (input) => {setSection(input)}
+  
 
   const [movies, setMovies] = useState()
   let getMovie = axios.get("https://mock-api.driven.com.br/api/v8/cineflex/movies")
@@ -28,9 +29,22 @@ function App() {
   <>
   <Header/>
   <Section section={section}/>
+  <MovieCatalog>
+   
   {movies ? movies.map((n) => <SelectMovie movieId={n.id} movieTitle={n.title} moviePoster={n.posterURL} movieOverview={n.overview} movieReleaseDate={n.releaseDate} />) : <Loading />}
+    
+  </MovieCatalog>
   </>
   );
 }
+
+const MovieCatalog = styled.div `
+width: 90%;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-around;
+margin-left: 12%;
+margin-right: 12%;
+`
 
 export default App;
--- src/components/Movies/SelectMovie.jsx
+++ src/components/Movies/SelectMovie.jsx
@@ -6,8 +6,6 @@ export default function SelectMovie(prop) {
  console.log(prop.moviePoster)
   return (
     <Container>
-      {prop.movieId}
-      {prop.movieTitle}
       <img src={prop.moviePoster}/>
     </Container>
     );
@@ -18,13 +16,14 @@ export default function SelectMovie(prop) {
     height: 209px;
     left: 205px;
     top: 389px;
-    margin-left: 30px;
     background: #FFFFFF;
     box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
     border-radius: 3px;
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-right: 25%;
+    margin-bottom: 11px;
       img{
         width: 129px;
         height: 193px;
